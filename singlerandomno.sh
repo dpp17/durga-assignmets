e903adf7a816d6a3258371fca201a618ac7e1b1d
dpp17 <durga123q.com>
2023-01-03 19:49:55 +0530

    Added: random func used to get single digit.

--- singlerandomno.sh
+++ singlerandomno.sh
@@ -1,5 +1,5 @@
-echo " ****** USE RANDOM FUNCTION TO GET SINGLE DIGIT ****** "
+echo " ****** USE RANDOM_FUNCTION TO GET SINGLE DIGIT ****** "
 printf "\n"
-digit=$(($RANDOM%10))
+digit=$(($RANDOM%10)) #random digit taken
 printf "\n"
 echo "Single_Digit :: " $digit
